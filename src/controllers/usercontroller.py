b3a957e51ed0b3a247bfa916572065c5541b2479
papichulo666 <23308060610438@cetis61.edu.mx>
2026-04-13 10:52:16 -0600

    dd

---
+++ src/controllers/usercontroller.py
@@ -0,0 +1,14 @@
+from src.models.UserModel import UsuarioModel
+from src.models.schemasModel import UsuarioSchemas
+
+class AuthController:
+    def __init__(self):
+        self.model=UsuarioModel()
+        def registrar-ususario(self, nombre, email, password):
+            try:
+                nuevo_usuario = UsuarioSchemas(nombre=nombre, email=email, password=password)
+                succes = self.model.registrar(nuevo_usuario)
+                return succes, "Usuario creado correctamente"
+            except ValidationError as e:
+                return False, e,error()[0]['msg']
+                
