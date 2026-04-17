02913dd0d6c41bcf29c4171e6cf5d7c288fb0e71
papichulo666 <23308060610438@cetis61.edu.mx>
2026-04-17 10:02:53 -0500

    SS

---
+++ src/views/LoginView.py
@@ -0,0 +1,29 @@
+import flet as ft 
+
+def LoginView(page, auth_controller):
+    email_input = ft.TextField(label="correo electronico", width=350, border_radius=10)
+    pass_input = ft.TextField(label="contraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
+    
+    def login_click(e):
+        user, msg = auth_controller.login(email_input.value, pass_input,value)
+        if user:
+            page.session.set("user", user)
+            page.go("/dashboard")
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text(msg))
+            page.snack_bar.open = True
+            page.update()
+            
+    return ft.View("/", [
+        ft.AppBar(title=ft.Text("SIGE - Login"), bgvolor=ft.colors.BLUE_GREY_900, color="white"),
+        ft.column([
+            ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.colors.BLUE),
+            ft.Text("Acceso al sistema", size=20, weight="bold"),
+            email_input,
+            pass_input
+            ft.ElevatedButton("Entrar", on_click=login_click, width=350),
+            ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
+            
+        ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER)
+            
+    ])
