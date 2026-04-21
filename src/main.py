6e7d3345d75e80debc3b0dea216ae368b5eaa4c1
papichulo666 <23308060610438@cetis61.edu.mx>
2026-04-21 08:24:48 -0500

    ss

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import flet as ft 
+from controllers.usercontroller import AuthController
+from controllers.tareacontroller import TareaController
+from Views.LoginView import LoginView
+from Views.DashboardView import DashboardView
+
+def start(page: ft.page):
+    
+    page.title = "Sistema SIGE"
+    page.window_width = 450 
+    page.window_height = 700
+    
+    auth_ctrl = AuthController()
+    task_ctrl = TareaController()
+    
+    def route_change(e):
+        page.Views.clear()
+        
+        if page.route == "/":
+            page.Views.append(LoginView(page, auth_ctrl))
+            
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+            
+            page.update()
+            
+            def view_pop(e):
+            if len(page.views) > 1: 
+            page.views.pop()
+            top_view = page.views[-1]
+            page.go(top_view.route)
+                    
+            page.on_route_change = route_change
+            page.on_view_pop = view_pop
+                    
+            print("iniciando navegacion...")
+            if page.route =="/",
+                    route_change(None)
+            else:
+                    page.go("/")
+                    
+            def main():
+                        ft.app(target=start)
+                        
+if __name__ == "__main__":
+        main()
